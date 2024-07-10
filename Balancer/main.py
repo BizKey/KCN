cf8e9b70d15862c0d5ef025d97b9e8e3e616048e
kizyanov <kizyanov@protonmail.com>
2024-07-10 17:06:11 -0400

    add sent balance on start Balance service

--- Balancer/main.py
+++ Balancer/main.py
@@ -1,15 +1,32 @@
 import asyncio
 import nats
+import orjson
 import uvloop
 from kucoin.client import WsToken
 from kucoin.ws_client import KucoinWsClient
 from loguru import logger
-from kucoin.client import WsToken
+from kucoin.client import WsToken, User, Market
 from decouple import config
 
 passphrase = config("PASSPHRASE", cast=str)
 key = config("KEY", cast=str)
 secret = config("SECRET", cast=str)
+
+user = User(
+    key=key,
+    secret=secret,
+    passphrase=passphrase,
+    is_sandbox=False,
+)
+
+
+market = Market(
+    key=key,
+    secret=secret,
+    passphrase=passphrase,
+    is_sandbox=False,
+)
+
 
 client = WsToken(
     key=key,
@@ -18,11 +35,33 @@ client = WsToken(
     url="https://openapi-v2.kucoin.com",
 )
 
+order_book = {
+    f"{sh['currency']}": {"available": sh["available"]}
+    for sh in user.get_account_list(account_type="margin")
+}
+
+
+for symbol in market.get_symbol_list_v2():
+    if symbol in order_book:
+        order_book[symbol].update({"baseincrement": symbol["baseIncrement"]})
+
 
 async def main():
     nc = await nats.connect("nats")
 
     js = nc.jetstream()
+
+    for symbol in order_book.keys():
+        await js.publish(
+            "candle",
+            orjson.dumps(
+                {
+                    "symbol": f"{symbol}-USDT",
+                    "baseincrement": order_book[symbol]["baseincrement"],
+                    "available": order_book[symbol]["available"],
+                }
+            ),
+        )
 
     async def event(msg: dict) -> None:
         logger.info(msg)
